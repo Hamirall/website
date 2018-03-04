9ca039919e3472ddabf4fd2b20555c12ce147940
Hamirall <32832060+Hamirall@users.noreply.github.com>
2018-03-04 22:12:50 +0100

    Mise sur GitHub de mon site

---
+++ Mon_site/index.php
@@ -0,0 +1,69 @@
+<?php include 'layout/header.php' ?>
+
+<img src="img/programmation.jpg" alt="image de programmation">
+
+<div class="pres">
+	<h1>Présentation</h1>
+		<hr>
+	<section>
+		<p>
+			Je m'appel François, j'ai 21 ans. Je suis étudiant en développement informatique plus particuliérement dans le web.
+			Je suis à la recherche d'un stage pour valider ma prémière année. Mon stage est d'une durée de 3 mois minimum, et 5 mois maximum.
+			Le stage commence le 3 avril et fini le 31 août.
+
+			Juste en dessous vous pouvez voir mes réalisations.
+
+			Si vous êtes intéressé, vous pouvez me contacter via l'onglet contact.
+		</p>
+	</section>
+</div>
+
+
+<div class="real">
+	<h1>Mes réalisations</h1>
+	<hr>
+		
+	<div class="card-deck">
+		  <div class="card">
+		    <img class="card-img-top" src="..." alt="Card image cap">
+		    <div class="card-body">
+		      <h5 class="card-title">Card title</h5>
+		      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		      <a href="#" class="btn btn-primary">Exemple</a>
+		    </div>
+		  </div>
+		  <div class="card">
+		    <img class="card-img-top" src="..." alt="Card image cap">
+		    <div class="card-body">
+		      <h5 class="card-title">Card title</h5>
+		      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+		      <a href="#" class="btn btn-primary">Exemple</a>
+		    </div>
+		  </div>
+		  <div class="card">
+		    <img class="card-img-top" src="..." alt="Card image cap">
+		    <div class="card-body">
+		      <h5 class="card-title">Card title</h5>
+		      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+		       <a href="#" class="btn btn-primary">Exemple</a>
+		    </div>
+		  </div>
+		</div>
+	</div>
+
+<div class="network">
+	<h3>Mes réseaux sociaux</h3>
+	<hr>
+
+	
+	<div class="img-center">
+		<img src="img/network/github.png" alt="logo GitHub">
+		<img src="img/network/twitter.png" alt="logo Twitter">
+		<img src="img/network/linkedin.png" alt="logo Linkedin">
+		<img src="img/network/youtube.png" alt="logo YouTube" class="logo-yt">
+	</div>
+	
+</div>
+
+
+<?php include 'layout/footer.php' ?>
---
+++ Mon_site/layout/footer.php
@@ -0,0 +1,10 @@
+
+
+
+		</div>
+	</div>
+<body>
+
+<footer>
+	<p>copyright 2018 - Hamirall</p>
+</footer>
