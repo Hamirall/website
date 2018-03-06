1e1a6818c4d99e1ae4a0d3727b538941c128e09e
Hamirall <32832060+Hamirall@users.noreply.github.com>
2018-03-06 22:33:27 +0100

    Formulaire php fonctionnel
    - Ajout des fichiers config pour sendmail, pour l'envoi de mail en local

--- website/Mon_site/libs/mail.php
+++ website/Mon_site/libs/mail.php
@@ -6,23 +6,46 @@ function veriInput() {
 	
 	if (!empty($_POST['email']) && !empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['message'])) {
 		
-		header("Location:../index.php");
-		$_SESSION['success_message'] = "Votre message à bien été envoyé";
+		email();
 	} 
 
 	else {
-		header("Location:../index.php");
+		header("Location: ../index.php");
 		$_SESSION['error_message'] = "Merci de saisir toutes les données";
 	}
 
 }
 
 
-function envoiMail() {
+function email() {
 
-	var_dump("mail");
+ 	
+ 	$expediteur = stripslashes($_POST['email']); 
+    $nom = stripslashes($_POST['nom']); 
+    $prenom = stripslashes($_POST['prenom']);  
+    $message = stripslashes($_POST['message']); 
 
+    /* Destinataire (votre adresse e-mail) */
+$to = 'testfonctionmail@gmail.com';
+ 
+/* Construction du message */
+$msg  = 'Bonjour,'. $nom ."\n";
+$msg .= 'Ce mail a été envoyé depuis Hamirall.com par : '.$nom."\n\n";
+$msg .= 'Voici le message qui vous est adressé :'."\n";
+$msg .= '***************************'."\n";
+$msg .= $message."\n";
+$msg .= '***************************'."\n";
+ 
+/* En-têtes de l'e-mail */
+$headers = 'From: <'.$expediteur.">\n";
+/* Envoi de l'e-mail */
+mail($to, $msg, $headers);
+
+header("Location: ../index.php");
+$_SESSION['success_message'] = "Votre message à bien été envoyé";
 }
+
+
 
 veriInput();
 
